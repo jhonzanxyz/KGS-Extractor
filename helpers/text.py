e9c912f07fa42598ed6ee38fc475a089e3c84674
jhonzanxyz <jhonzanxyz@gmail.com>
2025-04-17 20:47:36 +0530

    Update text.py

--- helpers/text.py
+++ helpers/text.py
@@ -1,9 +1,9 @@
 start_text = """
-▰▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▰
-💠 Hello Sir, I'm KGS Extractor Bot.
+----------------------------------------
+Hello Sir, I'm KGS Extractor Bot.
 
-➭ Developed By : Crazy_Mind
-▰▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▰
+-> Developed By : Crazy_Mind
+----------------------------------------
 
-➭ Press /khan to extract all downloadable links of Khan Global Studies (KGS) using ID & Password.
+-> Press /khan to extract all downloadable links of Khan Global Studies (KGS) using ID & Password.
 """
